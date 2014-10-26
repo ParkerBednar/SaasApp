917cb75eb5f74fab1736d9de0dc7255bdf59469f
parkerbednar <pbednar14@gmail.com>
2014-10-26 20:03:02 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaasApp::Application.config.secret_token = '6789293a7dba486992bc09d208eea49c5800ca7d9bd90aab960be548f9bd91635ce8cd9fb008d03de7827b65928e4b2962ab0b0d9aeb412d798f9479006fef39'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SaasApp::Application.config.session_store :cookie_store, key: '_SaasApp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SaasApp::Application.config.session_store :active_record_store
